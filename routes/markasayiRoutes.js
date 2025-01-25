94c00ab2d457887395b721a60527eae52fd3b988
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:03:27 +0300

    Update markasayiRoutes.js

--- routes/markasayiRoutes.js
+++ routes/markasayiRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getTotalMarkasayi } = require('../controllers/markasayiController');
 
-// Toplam marka sayısı endpoint'i
 router.get('/totalmarkasayi', getTotalMarkasayi);
 
 module.exports = router;

5100e171c4793b45b8c116063885fe998333e528
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:02:24 +0300

    Update ekipperformansRoutes.js

--- routes/ekipperformansRoutes.js
+++ routes/ekipperformansRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ekipperformansController = require('../controllers/ekipperformansController');
 
-// Kampanya verilerini almak için route
 router.get('/ekip-performans', ekipperformansController.getEkipData);
 
 module.exports = router;

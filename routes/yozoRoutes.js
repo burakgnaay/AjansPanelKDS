2992dc50adc7338be718a96efca5176eb1a2c593
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:05:11 +0300

    Update yozoRoutes.js

--- routes/yozoRoutes.js
+++ routes/yozoRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const yozoController = require('../controllers/yozoController');
 
-// "/markalar" URL'si ile yozo.html'e yönlendir
 router.get('/yozoUrban', yozoController.showYozoPage);
 
 module.exports = router;

7c3b0814d0282606f4e532cbbeb4d6ac074b33cd
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:01:56 +0300

    Update ekipkarsilastirmaRoutes.js

--- routes/ekipkarsilastirmaRoutes.js
+++ routes/ekipkarsilastirmaRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getEkipkarsilastirmaData } = require('../controllers/ekipkarsilastirmaController');  // Controller'ı dahil et
+const { getEkipkarsilastirmaData } = require('../controllers/ekipkarsilastirmaController');  
 
-// Pie chart verisini döndüren route
 router.get('/ekipkarsilastirmadata', getEkipkarsilastirmaData);
 
 module.exports = router;

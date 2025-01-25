16d652c1492806065a74af410fdddc9a9e9f1f96
burakgnaay <gunayburak14@outlook.com>
2025-01-25 17:56:32 +0300

    Update icerikController.js

--- controllers/icerikController.js
+++ controllers/icerikController.js
@@ -1,4 +1,4 @@
-// Sabit verilerinizi const olarak tanımlıyoruz
+
 const icerikData = [
     { icerik_tur_ad: "1-Video", toplam_gelir: 498106 },
     { icerik_tur_ad: "2-Görsel", toplam_gelir: 326755 },
@@ -8,7 +8,7 @@ const icerikData = [
 
 exports.getIcerikPerformans = async (req, res) => {
     try {
-        // Sabit verileri JSON formatında frontend'e gönderiyoruz
+     
         res.json(icerikData);
     } catch (err) {
         console.error("Veri Hatası:", err);

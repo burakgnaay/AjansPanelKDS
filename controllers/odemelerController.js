dd4d7c9c6a40624b69e60ceeb11076a066c81388
burakgnaay <gunayburak14@outlook.com>
2025-01-25 17:58:31 +0300

    Update odemelerController.js

--- controllers/odemelerController.js
+++ controllers/odemelerController.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 exports.getOdemeler = (req, res) => {
-    const filePath = path.join(__dirname, '../views/odemeler.html'); // views klasöründeki odemeler.html dosyasını buluyoruz
+    const filePath = path.join(__dirname, '../views/odemeler.html'); 
     res.sendFile(filePath);
 };

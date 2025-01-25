ae6c6c03066f7241f8898ab3915eb94ef8057568
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:04:55 +0300

    Update spentRoutes.js

--- routes/spentRoutes.js
+++ routes/spentRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const spentController = require('../controllers/spentController');
 
-// Toplam harcama endpoint
 router.get('/totalspent', spentController.getTotalSpent);
 
 module.exports = router;

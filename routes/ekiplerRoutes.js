0dc6693653f273dcb22ed1acff0e9cc3261990c1
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:02:15 +0300

    Update ekiplerRoutes.js

--- routes/ekiplerRoutes.js
+++ routes/ekiplerRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const ekiplerController = require('../controllers/ekiplerController'); // doğru yolu belirtiyoruz
+const ekiplerController = require('../controllers/ekiplerController'); 
 
-// Ekipler sayfasına yönlendiren route
 router.get('/ekipler', ekiplerController.getEkipler);
 
 module.exports = router;

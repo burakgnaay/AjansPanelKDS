34b209401eda1e763e9b46abb66695cb6c554c67
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:04:21 +0300

    Update platformRoutes.js

--- routes/platformRoutes.js
+++ routes/platformRoutes.js
@@ -1,10 +1,8 @@
-// roiRoutes.js
 const express = require("express");
 const { getPlatformPerformans } = require("../controllers/platformController");
 
 const router = express.Router();
 
-// Burada doğru rota tanımlandığından emin olun
 router.get("/platform-performans", getPlatformPerformans);
 
 module.exports = router;

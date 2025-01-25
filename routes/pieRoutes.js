5b4489e49aff554e3ef45a3dbcc96b07f9579581
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:04:07 +0300

    Update pieRoutes.js

--- routes/pieRoutes.js
+++ routes/pieRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getPieData } = require('../controllers/pieController');  // Controller'ı dahil et
+const { getPieData } = require('../controllers/pieController'); 
 
-// Pie chart verisini döndüren route
 router.get('/piedata', getPieData);
 
 module.exports = router;

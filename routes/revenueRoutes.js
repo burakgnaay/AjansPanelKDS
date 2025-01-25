94728f7a0d48291d105c71a60b8baac299daef0d
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:04:31 +0300

    Update revenueRoutes.js

--- routes/revenueRoutes.js
+++ routes/revenueRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const revenueController = require('../controllers/revenueController');
 
-// Toplam kazanc endpoint
 router.get('/totalrevenue', revenueController.getTotalRevenue);
 
 module.exports = router;

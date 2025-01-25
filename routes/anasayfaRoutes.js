8989271a45bcb2b3d02d04b621cda613a48de219
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:01:40 +0300

    Update anasayfaRoutes.js

--- routes/anasayfaRoutes.js
+++ routes/anasayfaRoutes.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 const anasayfaController = require('../controllers/anasayfaController');
 
-// "/markalar" URL'si ile yozo.html'e yönlendir
 router.get('/anasayfa', anasayfaController.showAnasayfaPage);
 
 module.exports = router;

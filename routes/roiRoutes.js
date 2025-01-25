f4d5ff67061e59444f2807dc8a796c0c590fdcf6
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:04:43 +0300

    Update roiRoutes.js

--- routes/roiRoutes.js
+++ routes/roiRoutes.js
@@ -1,10 +1,8 @@
-// roiRoutes.js
 const express = require("express");
 const { getMonthlyROI } = require("../controllers/roiController");
 
 const router = express.Router();
 
-// Burada doğru rota tanımlandığından emin olun
 router.get("/monthly-roi", getMonthlyROI);
 
 module.exports = router;

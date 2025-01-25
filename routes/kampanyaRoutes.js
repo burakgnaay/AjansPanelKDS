3c23106e07ce83e8444cdadc53091a7a018a7685
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:03:04 +0300

    Update kampanyaRoutes.js

--- routes/kampanyaRoutes.js
+++ routes/kampanyaRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const kampanyaController = require('../controllers/kampanyaController');
 
-// Kampanya verilerini almak için route
 router.get('/kampanya-performans', kampanyaController.getKampanyaData);
 
 module.exports = router;

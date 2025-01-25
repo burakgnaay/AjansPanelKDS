360566be8231e15f5994035c1421f4d2ed64695b
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:03:39 +0300

    Update odemebilgiRoutes.js

--- routes/odemebilgiRoutes.js
+++ routes/odemebilgiRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const odemebilgiController = require('../controllers/odemebilgiController');
 
-// Kampanya verilerini almak için route
 router.get('/odeme-bilgi', odemebilgiController.getOdemebilgiData);
 
 module.exports = router;

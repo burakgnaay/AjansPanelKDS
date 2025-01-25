47910ccc5696954e2276710042fadbee4ef4f5cc
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:00:44 +0300

    Update yozoController.js

--- controllers/yozoController.js
+++ controllers/yozoController.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 exports.showYozoPage = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/yozo.html')); // yozo.html'in doğru yolu
+    res.sendFile(path.join(__dirname, '../views/yozo.html')); 
 };

6b44bf8c774c909a11070606cdd9207bfa06d43c
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:34:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
-const anasayfaRoutes = require('./routes/anasayfaRoutes'); // Anasayfa rotasını içe aktarın
-const db = require('./db'); // Veritabanı bağlantınızı burada yapıyorsunuz
-const yozoRoutes = require('./routes/yozoRoutes'); // Yozo route'larını ekliyoruz
+const anasayfaRoutes = require('./routes/anasayfaRoutes'); 
+const db = require('./db');
+const yozoRoutes = require('./routes/yozoRoutes'); 
 const ekiplerRoutes = require('./routes/ekiplerRoutes');
-const odemelerRoutes = require('./routes/odemelerRoutes'); // Ödemeler route'ını içe aktarın
+const odemelerRoutes = require('./routes/odemelerRoutes'); 
 const userRoutes = require('./routes/userRoutes');
 const roiRoutes = require('./routes/roiRoutes');
 const spentRoutes = require('./routes/spentRoutes');
 const revenueRoutes = require('./routes/revenueRoutes');
 const markasayiRoutes = require('./routes/markasayiRoutes');
-const pieRoutes = require('./routes/pieRoutes');  // pieRoutes.js'i dahil et
+const pieRoutes = require('./routes/pieRoutes'); 
 const kampanyaRoutes = require('./routes/kampanyaRoutes'); 
 const icerikRoutes = require('./routes/icerikRoutes');
 const kampanyaturRoutes = require('./routes/kampanyaturRoutes');
@@ -21,12 +21,12 @@ const ekipperformansRoutes = require('./routes/ekipperformansRoutes');
 const ekipkarsilastirmaRoutes = require('./routes/ekipkarsilastirmaRoutes');
 const odemebilgiRoutes = require('./routes/odemebilgiRoutes');
 
-const app = express(); // `app` değişkenini en başta tanımlayın
+const app = express(); 
 
-// Statik dosya dizinini tanımlayın
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware'ler
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
@@ -38,19 +38,19 @@ app.use(
 );
 app.use(express.json());
 
-// Statik dosyaları tanımla
 
 
 
-// Rotalar
+
+
 app.get('/', (req, res) => {
-    res.redirect('/login'); // localhost:3000 -> /login yönlendirmesi
+    res.redirect('/login'); 
 });
-app.use('/', authRoutes); // Auth rotalarını bağlayın
-app.use('/', anasayfaRoutes); // Anasayfa rotasını bağlayın
+app.use('/', authRoutes); 
+app.use('/', anasayfaRoutes); 
 app.use('/', yozoRoutes);
 app.use('/', ekiplerRoutes);
-app.use('/', odemelerRoutes); // Ödemeler route'ını bağlayın
+app.use('/', odemelerRoutes); 
 app.use(userRoutes);
 app.use('/roi' , roiRoutes);
 app.use(spentRoutes);
@@ -68,7 +68,6 @@ app.use('/odeme', odemebilgiRoutes);
 
 
 
-// Sunucuyu başlat
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);

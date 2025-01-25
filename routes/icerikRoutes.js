58da6cf0b1ffbc400a353dc251a0fd4a148d4f44
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:02:48 +0300

    Update icerikRoutes.js

--- routes/icerikRoutes.js
+++ routes/icerikRoutes.js
@@ -1,10 +1,8 @@
-// roiRoutes.js
 const express = require("express");
 const { getIcerikPerformans } = require("../controllers/icerikController");
 
 const router = express.Router();
 
-// Burada doğru rota tanımlandığından emin olun
 router.get("/icerik-performans", getIcerikPerformans);
 
 module.exports = router;

e0218974126e09cf0931c6da187cccec31665695
burakgnaay <gunayburak14@outlook.com>
2025-01-25 17:59:42 +0300

    Update revenueController.js

--- controllers/revenueController.js
+++ controllers/revenueController.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
 
-// Veritabanı bağlantısı
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -13,7 +12,6 @@ db.connect((err) => {
     console.log('Veritabanı bağlantısı başarılı!');
 });
 
-// Toplam harcama sorgusu
 const getTotalRevenue = (req, res) => {
     const query = 'SELECT SUM(revenue) AS totalRevenue FROM kampanya_performans';
     db.query(query, (err, result) => {

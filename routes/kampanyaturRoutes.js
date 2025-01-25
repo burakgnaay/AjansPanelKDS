bd7c4b9b11853b7b205867f246c7ee04490b77e2
burakgnaay <gunayburak14@outlook.com>
2025-01-25 18:03:17 +0300

    Update kampanyaturRoutes.js

--- routes/kampanyaturRoutes.js
+++ routes/kampanyaturRoutes.js
@@ -1,10 +1,8 @@
-// roiRoutes.js
 const express = require("express");
 const { getKampanyaturPerformans } = require("../controllers/kampanyaturController");
 
 const router = express.Router();
 
-// Burada doğru rota tanımlandığından emin olun
 router.get("/kampanyatur-performans", getKampanyaturPerformans);
 
 module.exports = router;
